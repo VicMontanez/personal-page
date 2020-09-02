2db18a0d1ed74b3ff12ed218dd489279df425be2
VicMontanez <v_montanez@hotmail.com>
2020-09-02 00:12:24 -0400

    set menu state

--- dist/js/main.js
+++ dist/js/main.js
@@ -11,12 +11,15 @@ let showMenu = false;
 menuBtn.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
-  if (!!showMenu) {
+  if (!showMenu) {
     menuBtn.classList.add("close");
     menu.classList.add("show");
     menuNav.classList.add("show");
     menuBranding.classList.add("show");
     navItems.forEach((item) => item.classList.add("show"));
+
+    //Set Menu State
+    showMenu = true;
   } else {
   }
 }
